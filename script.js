8bf3844855c0bc1dc0ab73224a88869f176e2f9e
codeheal <adeniyiridwan95@gmail.com>
2022-08-08 16:04:04 +0000

    java project

---
+++ script.js
@@ -0,0 +1,49 @@
+//number1
+ let quote =
+   "'There is no exercise better for the heart than reaching down and lifting people up.'";
+
+   console.log(quote);
+
+
+//number2
+quote =  
+"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+
+console.log(quote);
+
+
+//number3
+console.log(typeof "10" === 10);
+const number = 10;
+const string = "10";
+console.log(number == string);
+
+//num4
+let number = 9.8
+let float = number;
+console.log(math.ceil(num));
+
+//numb5
+console.log("phyton".includes("on") && "jargon".includes("on"));
+
+//numb6
+console.log("I hope this course is not full of jargon".includes("jargon"));
+
+//numb7
+let number = math.floor(math.random() * 101);
+console.log(number);
+
+//numb8
+let num = math.floor(math.random() * 50) + 50;
+console.log(number);
+
+
+
+
+
+//num12
+const sentence =
+"You cannot end a sentence with because because because is a conjuction";
+console.log(sentence.substf(31, 23));
+
+
